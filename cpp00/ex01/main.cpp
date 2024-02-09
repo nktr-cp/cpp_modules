77666627252b1397a927199917653e59425a5e43
nktr-cp <nktrcp@gmail.com>
2024-02-09 16:53:53 +0900

    Modified cpp00-ex01

--- cpp00/ex01/main.cpp
+++ cpp00/ex01/main.cpp
@@ -2,10 +2,19 @@
 #include <string>
 #include "PhoneBook.h"
 
-bool is_number(const std::string& s) {
+static bool _is_number(const std::string& s) {
 	std::string::const_iterator it = s.begin();
 	while (it != s.end() && std::isdigit(*it)) it++;
 	return (!s.empty() && it == s.end());
+}
+
+static void _process_input(std::string& s) {
+	std::getline(std::cin, s);
+	if (std::cin.fail() || std::cin.eof()) {
+		std::cout << std::endl;
+		std::cout << "EOF detected." << std::endl;
+		exit(EXIT_FAILURE);
+	}
 }
 
 static void _search(PhoneBook &phonebook) {
@@ -19,8 +28,12 @@ static void _search(PhoneBook &phonebook) {
 	std::string s_index;
 	std::cout << "Enter index: ";
 	std::cin >> s_index;
+	if (std::cin.eof()) {
+		std::cout << std::endl << "EOF detected." << std::endl;
+		exit(EXIT_FAILURE);
+	}
 	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-	if (std::cin.fail() || !is_number(s_index)) {
+	if (std::cin.fail() || !_is_number(s_index)) {
 		std::cout << "Invalid input" << std::endl;
 		std::cin.clear();
 		return;
@@ -37,19 +50,19 @@ static void _add(PhoneBook &phonebook) {
 	bool invalid = false;
 	std::string first_name, last_name, nickname, phone_number, darkest_secret;
 	std::cout << "Enter first name: ";
-	std::getline(std::cin, first_name);
+	_process_input(first_name);
 	invalid |= first_name.empty();
 	std::cout << "Enter last name: ";
-	std::getline(std::cin, last_name);
+	_process_input(last_name);
 	invalid |= last_name.empty();
 	std::cout << "Enter nickname: ";
-	std::getline(std::cin, nickname);
+	_process_input(nickname);
 	invalid |= nickname.empty();
 	std::cout << "Enter phone number: ";
-	std::getline(std::cin, phone_number);
+	_process_input(phone_number);
 	invalid |= phone_number.empty();
 	std::cout << "Enter darkest secret: ";
-	std::getline(std::cin, darkest_secret);
+	_process_input(darkest_secret);
 	invalid |= darkest_secret.empty();
 
 	if (invalid) {
@@ -68,7 +81,7 @@ int main() {
 		std::cout << "Enter command: ";
 		std::getline(std::cin, command);
 		if (std::cin.eof())
-			return 0;
+			exit(EXIT_FAILURE);
 		if (command == "ADD") {
 			_add(phonebook);
 		} else if (command == "SEARCH") {
