d151906d8a3294f08adbb2e830d28bcce7909d80
nktr-cp <nktrcp@gmail.com>
2024-04-29 20:22:47 +0900

    [Modified] cpp05-ex03

--- cpp05/ex03/main.cpp
+++ cpp05/ex03/main.cpp
@@ -3,47 +3,63 @@
 
 signed main(void) {
 	{
-		Bureaucrat bureauro("bureauro", 1);
-		Intern it;
+		try {
+			Bureaucrat bureauro("bureauro", 1);
+			Intern it;
 
-		AForm* form = it.makeForm("robotomy request", "Bender");
-		if (form) {
-			bureauro.signForm(*form);
-			bureauro.executeForm(*form);
-			delete form;
+			AForm* form = it.makeForm("robotomy request", "Bender");
+			if (form) {
+				bureauro.signForm(*form);
+				bureauro.executeForm(*form);
+				delete form;
+			}
+		} catch (std::exception& e) {
+			std::cout << e.what() << std::endl;
 		}
 	}
 	{
-		Bureaucrat bureauro("bureauro", 1);
-		Intern it;
+		try {
+			Bureaucrat bureauro("bureauro", 1);
+			Intern it;
 
-		AForm* form = it.makeForm("shrubbery creation", "Bender");
-		if (form) {
-			bureauro.signForm(*form);
-			bureauro.executeForm(*form);
-			delete form;
+			AForm* form = it.makeForm("shrubbery creation", "Bender");
+			if (form) {
+				bureauro.signForm(*form);
+				bureauro.executeForm(*form);
+				delete form;
+			}
+		} catch (std::exception &e) {
+			std::cout << e.what() << std::endl;
 		}
 	}
 	{
-		Bureaucrat bureauro("bureauro", 1);
-		Intern it;
+		try {
+			Bureaucrat bureauro("bureauro", 1);
+			Intern it;
 
-		AForm* form = it.makeForm("presidential pardon", "Bender");
-		if (form) {
-			bureauro.signForm(*form);
-			bureauro.executeForm(*form);
-			delete form;
+			AForm* form = it.makeForm("presidential pardon", "Bender");
+			if (form) {
+				bureauro.signForm(*form);
+				bureauro.executeForm(*form);
+				delete form;
+			}
+		} catch (std::exception &e) {
+			std::cout << e.what() << std::endl;
 		}
 	}
 	{
-		Bureaucrat bureauro("bureauro", 1);
-		Intern it;
+		try {
+			Bureaucrat bureauro("bureauro", 1);
+			Intern it;
 
-		AForm* form = it.makeForm("dummy form", "Bender");
-		if (form) {
-			bureauro.signForm(*form);
-			bureauro.executeForm(*form);
-			delete form;
+			AForm* form = it.makeForm("dummy form", "Bender");
+			if (form) {
+				bureauro.signForm(*form);
+				bureauro.executeForm(*form);
+				delete form;
+			}
+		} catch (std::exception &e) {
+			std::cout << e.what() << std::endl;
 		}
 	}
 }
