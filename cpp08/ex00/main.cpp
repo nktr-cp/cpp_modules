fa1070671b4eb049c1ff1f357ef2bd05e6b17ae7
nktr-cp <nktrcp@gmail.com>
2024-05-03 17:57:30 +0900

    [Modified] cpp08-ex00

--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -4,33 +4,37 @@
 #include "easyfind.hpp"
 
 signed main(void) {
-	std::vector<int> vec;
-	for (int i=0; i<=20; i+=2) {
-		vec.push_back(i);
-	}
+	try {
+		std::vector<int> vec;
+		for (int i=0; i<=20; i+=2) {
+			vec.push_back(i);
+		}
 
-	std::cout << "Elements of vector" << std::endl;
-	for (size_t i=0; i<vec.size(); i++) {
-		std::cout << "+++++++++++++++" << std::endl;
-		std::cout << "index: " << i << std::endl;
-		std::cout << "value: " << vec[i] << std::endl;
-	}
+		std::cout << "Elements of vector" << std::endl;
+		for (size_t i=0; i<vec.size(); i++) {
+			std::cout << "+++++++++++++++" << std::endl;
+			std::cout << "index: " << i << std::endl;
+			std::cout << "value: " << vec[i] << std::endl;
+		}
 
-	{
-		std::vector<int>::iterator it = easyfind(vec, 10);
+		{
+			std::vector<int>::iterator it = easyfind(vec, 10);
 
-		std::cout << "+++++++++++++++" << std::endl;
-		std::cout << "Result of easyfind(vec, 10)" << std::endl;
-		std::cout << it - vec.begin() << std::endl;
-		std::cout << *it << std::endl;
-	}
-	{
-		std::vector<int>::iterator it = easyfind(vec, 100);
-		assert(it == vec.end());
+			std::cout << "+++++++++++++++" << std::endl;
+			std::cout << "Result of easyfind(vec, 10)" << std::endl;
+			std::cout << it - vec.begin() << std::endl;
+			std::cout << *it << std::endl;
+		}
+		{
+			std::vector<int>::iterator it = easyfind(vec, 100);
+			assert(it == vec.end());
 
-		std::cout << "+++++++++++++++" << std::endl;
-		std::cout << "Result of easyfind(vec, 100)" << std::endl;
-		std::cout << it - vec.begin() << std::endl;
-		// std::cout << *it << std::endl;
+			std::cout << "+++++++++++++++" << std::endl;
+			std::cout << "Result of easyfind(vec, 100)" << std::endl;
+			std::cout << it - vec.begin() << std::endl;
+			// std::cout << *it << std::endl;
+		}
+	} catch (std::exception &e) {
+		std::cout << "Exception thrown: " << e.what() << std::endl;
 	}
 }
