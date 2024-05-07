175aa978c154aa087b5fc8ff4fb2c32ea199056b
nktr-cp <nktrcp@gmail.com>
2024-05-07 19:46:42 +0900

    [Modified] cpp02-ex03

--- cpp02/ex03/bsp.cpp
+++ cpp02/ex03/bsp.cpp
@@ -1,18 +1,28 @@
 #include "Point.h"
 // https://www.geeksforgeeks.org/check-whether-a-given-point-lies-inside-a-triangle-or-not/
 
-static float area(Point const p1, Point const p2, Point const p3) {
-	float x1 = p1.getX().toFloat();
-	float y1 = p1.getY().toFloat();
-	float x2 = p2.getX().toFloat();
-	float y2 = p2.getY().toFloat();
-	float x3 = p3.getX().toFloat();
-	float y3 = p3.getY().toFloat();
+static float cross(Point const a, Point const b, Point const c) {
+	float ax = b.getX().toFloat() - a.getX().toFloat();
+	float ay = b.getY().toFloat() - a.getY().toFloat();
+	float bx = c.getX().toFloat() - a.getX().toFloat();
+	float by = c.getY().toFloat() - a.getY().toFloat();
+	return ax * by - ay * bx;
+}
 
-	return std::fabs(((x1*(y2-y3)) + x2*(y3-y1) + x3*(y1-y2)) * 0.5f);
+static bool not_on_edge_or_vertex(Point const &a, Point const &b, Point const &c, Point const &p) {
+	float ab = cross(a, b, p);
+	float bc = cross(b, c, p);
+	float ca = cross(c, a, p);
+	return std::fabs(ab) > 1e-6 && std::fabs(bc) > 1e-6 && std::fabs(ca) > 1e-6;
+}
+
+static float area(Point const p1, Point const p2, Point const p3) {
+	return std::fabs(cross(p1, p2, p3)) * 0.5f;
 }
 
 bool bsp(Point const a, Point const b, Point const c, Point const point) {
+	if (!not_on_edge_or_vertex(a, b, c, point)) return false;
+
 	float A = area(a, b, c);
 	float A1 = area(point, b, c);
 	float A2 = area(a, point, c);
